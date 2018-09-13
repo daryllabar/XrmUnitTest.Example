ecf350762d3038c974b101a7751a40599e14dce7
Daryl LaBar <d3labar@hotmail.com>
2018-09-12 23:38:53 -0400

    Initial Commit

---
+++ Xyz.Xrm.Plugin/Workflow/CreateGuidActivity.cs
@@ -0,0 +1,16 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Xyz.Xrm.Plugin.Workflow
+{
+    public class CreateGuidActivity : CodeActivityBase
+    {
+        [Output("Guid")]
+        public OutArgument<string> Guid { get; set; }
+
+        protected override void Execute(ExtendedWorkflowContext activityContext)
+        {
+            Guid.Set(activityContext, System.Guid.NewGuid().ToString());
+        }
+    }
+}
