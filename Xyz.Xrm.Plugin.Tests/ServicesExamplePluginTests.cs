cf38cf3ff03c0c1b9db51b391bb694370915d4b6
Daryl LaBar <d3labar@hotmail.com>
2024-01-13 23:59:06 -0500

    Added Testing for the Services Example Plugin

---
+++ Xyz.Xrm.Plugin.Tests/ServicesExamplePluginTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using DLaB.Xrm.Test.Builders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Source.DLaB.Xrm.Ioc;
+using System.Collections.Generic;
+using System.Linq;
+using Xyz.Xrm.Entities;
+
+namespace Xyz.Xrm.Plugin.Tests
+{
+    [TestClass]
+    public class ServicesExamplePluginTests
+    {
+        private ServicesExamplePlugin _sut;
+        private MockServicesExampleData _data;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var container = new IocContainer
+            {
+                PreBuildServiceProviderOverrideRegistrations = new IocContainer()
+            };
+            container.PreBuildServiceProviderOverrideRegistrations
+                .AddScoped<IServicesExampleData>(s => _data);
+
+            _data = new MockServicesExampleData();
+            _sut = new ServicesExamplePlugin(container);
+        }
+
+        [TestMethod]
+        public void ExamplePluginTestWithoutTestingDataverseAccess()
+        {
+            //
+            // Arrange
+            //
+            var mainContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                LastName = "Smith"
+            };
+            var context= new PluginExecutionContextBuilder()
+                .WithTarget(mainContact)
+                .Build();
+            var serviceProvider = new ServiceProviderBuilder(null, context).Build();
+            
+            //
+            // Act
+            //
+            _sut.Execute(serviceProvider);
+
+            //
+            // Assert
+            //
+            Assert.AreEqual(mainContact.LastName, _data.LastName);
+            Assert.AreEqual(mainContact.Id, _data.MainContact.Id);
+            Assert.AreEqual(_data.UpdatedContacts.Count, _data.Dependents.Count);
+            foreach (var expectedContact in _data.Dependents)
+            {
+                Assert.IsNotNull(_data.UpdatedContacts.Single(c => c.Id == expectedContact.Id));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Don't do this.  Use your favorite mocking framework to mock and verify these for testing
+    /// </summary>
+    public class MockServicesExampleData : IServicesExampleData
+    {
+        public List<Contact> Dependents { get; set; } = new List<Contact>
+        {
+            new Contact{ FirstName = "1", Id = Guid.NewGuid()},
+            new Contact{ FirstName = "2", Id = Guid.NewGuid()},
+            new Contact{ FirstName = "3", Id = Guid.NewGuid()},
+        };
+        public string LastName { get; private set; }
+        public Contact MainContact { get; private set; }
+        public List<Contact> UpdatedContacts { get; private set; }
+
+        public List<Contact> GetDependents(string lastName)
+        {
+            LastName = lastName;
+            return Dependents;
+        }
+
+        public void SyncAndUpdateAddresses(Contact main, List<Contact> dependents)
+        {
+            MainContact = main;
+            UpdatedContacts = dependents;
+        }
+}
+}
---
+++ Xyz.Xrm.Plugin/ServicesExamplePlugin.cs
@@ -0,0 +1,76 @@
+﻿using Source.DLaB.Xrm;
+using Source.DLaB.Xrm.Ioc;
+using Source.DLaB.Xrm.Plugin;
+using System.Collections.Generic;
+using System.Linq;
+using Xyz.Xrm.Entities;
+
+namespace Xyz.Xrm.Plugin
+{
+    /// <summary>
+    /// Just an example of how to utilize the IOC for plugins
+    /// </summary>
+    public class ServicesExamplePlugin : PluginBase
+    {
+        protected override IEnumerable<RegisteredEvent> CreateEvents()
+        {
+            return new RegisteredEventBuilder(PipelineStage.PreOperation, MessageType.Update).
+                ForEntities(Contact.EntityLogicalName).Build();
+        }
+
+        #region Constructors
+        public ServicesExamplePlugin(string unsecureConfig = null, string secureConfig = null) : base(unsecureConfig, secureConfig) { }
+        public ServicesExamplePlugin(IIocContainer container, string unsecureConfig = null, string secureConfig = null) : base(unsecureConfig, secureConfig, container)
+        {
+        }
+
+        #endregion Constructors
+
+        protected override void ExecuteInternal(ExtendedPluginContext context)
+        {
+            var data = context.ServiceProvider.GetService<IServicesExampleData>();
+            var contact = context.CoalesceTargetWithPreEntity<Contact>();
+            var dependents = data.GetDependents(contact.LastName);
+            data.SyncAndUpdateAddresses(contact, dependents);
+        }
+
+        protected override IIocContainer RegisterServices(IIocContainer container)
+        {
+            return base.RegisterServices(container)
+                .AddScoped<IServicesExampleData, DataLogicImplementation>();
+        }
+    }
+
+    public interface IServicesExampleData {
+        List<Contact> GetDependents(string lastName);
+        void SyncAndUpdateAddresses(Contact main, List<Contact> dependents);
+    }
+
+    public class DataLogicImplementation : IServicesExampleData {
+        private readonly ExtendedPluginContext _context;
+        public DataLogicImplementation(ExtendedPluginContext context)
+        {
+            _context = context;
+        }
+
+        public List<Contact> GetDependents(string lastName)
+        {
+            return _context.SystemOrganizationService.GetEntities<Contact>(Contact.Fields.LastName, lastName).ToList();
+        }
+
+        public void SyncAndUpdateAddresses(Contact main, List<Contact> dependents)
+        {
+            foreach(var dependent in dependents)
+            {
+                _context.OrganizationService.Update(new Contact{
+                    Address1_City = main.Address1_City,
+                    Address1_Line1 = main.Address1_Line1,
+                    Address1_Line2 = main.Address1_Line2,
+                    Address1_PostalCode = main.Address1_PostalCode,
+                    Address1_StateOrProvince = main.Address1_StateOrProvince,
+                    Id = dependent.Id
+                });
+            }
+        }
+    }
+}
