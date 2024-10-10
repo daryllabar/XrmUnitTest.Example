5140e6ba5ab07c4768cabc01d1a67060f8b96dda
Daryl LaBar <9728423+daryllabar@users.noreply.github.com>
2024-10-10 08:32:37 -0400

    Need to Set Proxy Types Assembly closes #10

--- Xyz.Xrm/Plugin/Extensions.cs
+++ Xyz.Xrm/Plugin/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using DLaB.Xrm;
 using DLaB.Xrm.Ioc;
 using DLaB.Xrm.Plugin;
+using Xyz.Xrm.Entities;
 
 namespace Xyz.Xrm.Plugin
 {
@@ -8,9 +10,20 @@ namespace Xyz.Xrm.Plugin
     {
         #region IocContainer
 
+        /// <summary>
+        /// Registers the default services for the IocContainer.
+        /// If different plugin assemblies require different IocContainer setups, then utilize the IPluginServicesRegistrationRecorder and the PluginServicesRegistrationRecorder assembly attribute
+        /// </summary>
+        /// <param name="container"></param>
+        /// <returns></returns>
         public static IIocContainer RegisterDefaultServices(this IIocContainer container)
         {
             // Register any custom services here
+            container.AddSingleton(_ => new ExtendedOrganizationServiceSettings
+            {
+                // If you're using EarlyBound, this would be any type the assembly that contains your early bound entities
+                ProxyTypesAssembly = typeof(DataverseContext).Assembly
+            });
             return container;
         }
 
